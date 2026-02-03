3adb3805d4e68c57f0ddf8e01dff62760e6f93b7
Technovahub Career <technovahubcareer@gmail.com>
2026-02-03 13:19:54 +0530

    img_added

--- src/BeforeAfter.jsx
+++ src/BeforeAfter.jsx
@@ -1,3 +1,28 @@
+// import beforeImg from "../assets/before.jpeg";
+// import afterImg from "../assets/after.jpeg";
+// const BeforeAfter = () => {
+//   return (
+//     <section className="container my-5">
+//       <h2 className="text-center mb-4">
+//         Before & After Transformation
+//       </h2>
+
+//       <div className="row g-4">
+//         <div className="col-md-6">
+//           <img src={beforeImg} className="img-fluid rounded" id="BAimg" />
+//         </div>
+//         <div className="col-md-6">
+//           <img src={afterImg} className="img-fluid rounded" id="BAimg" />
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default BeforeAfter;
+import beforeImg from "./assets/before.jpeg";
+import afterImg from "./assets/after.jpeg";
+
 const BeforeAfter = () => {
   return (
     <section className="container my-5">
@@ -7,10 +32,19 @@ const BeforeAfter = () => {
 
       <div className="row g-4">
         <div className="col-md-6">
-          <img src="src/assets/before.jpeg" className="img-fluid rounded" id="BAimg" />
+          <img
+            src={beforeImg}
+            alt="Before transformation"
+            className="img-fluid rounded BAimg"
+          />
         </div>
+
         <div className="col-md-6">
-          <img src="src/assets/after.jpeg" className="img-fluid rounded" id="BAimg" />
+          <img
+            src={afterImg}
+            alt="After transformation"
+            className="img-fluid rounded BAimg"
+          />
         </div>
       </div>
     </section>
